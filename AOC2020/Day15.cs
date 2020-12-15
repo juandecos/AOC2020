bc3fdfffc6a960288b69acd89d5dc9c7e054a052
Juan deCos <juandecos@gmail.com>
2020-12-15 01:05:56 -0500

    Day 15

---
+++ AOC2020/Day15.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace AOC2020
+{
+    [Day(15)]
+    class Day15 : Solver
+    {
+        public override object SolveOne()
+        {
+            return Solve(2020);
+        }
+
+        public override object SolveTwo()
+        {
+            return Solve(30000000);
+        }
+
+        int Solve(int count)
+        {
+            var data = Rows[0].Split(',').Select(x => int.Parse(x)).ToList();
+            var map = data.Select((x, i) => new { x, i }).ToDictionary(x => x.x, x => (Last: x.i, Prior: x.i));
+            int last = data.Last();
+            for (int i = data.Count; i < count; i++)
+            {
+                last = map.ContainsKey(last) ? map[last].Last - map[last].Prior : 0;
+                map[last] = (i, map.ContainsKey(last) ? map[last].Last : i);
+            }
+            return last;
+        }
+    }
+}
