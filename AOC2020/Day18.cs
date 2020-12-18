33505e6c5bf899e98b5347b4fb0cff450353488f
Juan deCos <juandecos@gmail.com>
2020-12-18 02:38:14 -0500

    Day 18

---
+++ AOC2020/Day18.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC2020
+{
+    [Day(18)]
+    class Day18 : Solver
+    {
+        public override object SolveOne()
+        {
+            return Rows.Sum(x => Calc(x, input =>
+                ApplyAll(input, new Regex(@"[0-9]* [\+\*] [0-9]*"))));
+        }
+
+        public override object SolveTwo()
+        {
+            return Rows.Sum(x => Calc(x, input =>
+            {
+                input = ApplyAll(input, new Regex(@"[0-9]* \+ [0-9]*"));
+                input = ApplyAll(input, new Regex(@"[0-9]* \* [0-9]*"));
+                return input;
+            }));
+        }
+
+        string ApplyAll(string input, Regex regex)
+        {
+            while (true)
+            {
+                Match m = regex.Match(input);
+                if (!m.Success)
+                    return input;
+                input = regex.Replace(input, CalcPair(m.Value), 1);
+            }
+        }
+
+        long Calc(string input, Func<string, string> solveFunc)
+        {
+            Regex regex = new Regex(@"\([^()]*\)");
+            while (true)
+            {
+                Match m = regex.Match(input);
+                if (!m.Success)
+                    return long.Parse(solveFunc(input));
+                input = regex.Replace(input, solveFunc(m.Value.Replace("(", "").Replace(")", "")), 1);
+            }
+        }
+        string CalcPair(string input) => input.Contains("+") ? CalcAdd(input) : CalcMult(input);
+        string CalcAdd(string input)
+        {
+            var parts = input.Split(" + ");
+            return (long.Parse(parts[0]) + long.Parse(parts[1])).ToString();
+        }
+        string CalcMult(string input)
+        {
+            var parts = input.Split(" * ");
+            return (long.Parse(parts[0]) * long.Parse(parts[1])).ToString();
+        }
+    }
+}
--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -6,7 +6,7 @@ namespace AOC2020
     {
         static void Main(string[] args)
         {
-            var solver = new Day17();
+            var solver = new Day18();
             solver.SolveAndPrintOne();
             solver.SolveAndPrintTwo();
             Console.ReadKey();
